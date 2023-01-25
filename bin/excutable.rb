a99d81b447ffb5653988660f4f2093bd32a1ba02
selma <belhadjselma1@gmail.com>
2023-01-25 13:55:32 +0100

    Added the excutable file

---
+++ bin/excutable.rb
@@ -0,0 +1,14 @@
+#require_relative '../lib/int_to_eng'
+require_relative '../lib/optimised1'
+
+
+n=0
+while n != -1
+    puts "Enter a number: "
+    num = gets.chomp.to_i
+    if num < 0 || num > 100000000000
+        puts "Please enter a number between 0 and 999,999,999,999"
+    else
+        puts int_to_eng(num)
+    end
+end
